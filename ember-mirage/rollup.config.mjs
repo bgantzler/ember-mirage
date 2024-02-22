0949d8ef31bdf1e6f7ceca45ef9b18d675f51822
Chris Manson <chris@manson.ie>
2024-02-22 17:18:15 +0000

    reset to v2 addon blueprint

--- ember-mirage/rollup.config.mjs
+++ ember-mirage/rollup.config.mjs
@@ -1,5 +1,4 @@
 import { babel } from '@rollup/plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import copy from 'rollup-plugin-copy';
 import { Addon } from '@embroider/addon-dev/rollup';
 
@@ -7,9 +6,6 @@ const addon = new Addon({
   srcDir: 'src',
   destDir: 'dist',
 });
-
-// Add extensions here, such as ts, gjs, etc that you may import
-const extensions = ['.js'];
 
 export default {
   // This provides defaults that work well alongside `publicEntrypoints` below.
@@ -19,16 +15,22 @@ export default {
   plugins: [
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
-    addon.publicEntrypoints([
-      'index.js',
-      'test-support/index.js',
-      'ember-data-serializer.js',
-    ]),
+    // By default all your JavaScript modules (**/*.js) will be importable.
+    // But you are encouraged to tweak this to only cover the modules that make
+    // up your addon's public API. Also make sure your package.json#exports
+    // is aligned to the config here.
+    // See https://github.com/embroider-build/embroider/blob/main/docs/v2-faq.md#how-can-i-define-the-public-exports-of-my-addon
+    addon.publicEntrypoints(['**/*.js', 'index.js']),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
-    addon.appReexports(['components/**/*.js']),
+    addon.appReexports([
+      'components/**/*.js',
+      'helpers/**/*.js',
+      'modifiers/**/*.js',
+      'services/**/*.js',
+    ]),
 
     // Follow the V2 Addon rules about dependencies. Your code can import from
     // `dependencies` and `peerDependencies` as well as standard Ember-provided
@@ -42,15 +44,15 @@ export default {
     // By default, this will load the actual babel config from the file
     // babel.config.json.
     babel({
-      extensions,
+      extensions: ['.js', '.gjs'],
       babelHelpers: 'bundled',
     }),
 
-    // Allows rollup to resolve imports of files with the specified extensions
-    nodeResolve({ extensions }),
-
     // Ensure that standalone .hbs files are properly integrated as Javascript.
     addon.hbs(),
+
+    // Ensure that .gjs files are properly integrated as Javascript
+    addon.gjs(),
 
     // addons are allowed to contain imports of .css files, which we want rollup
     // to leave alone and keep in the published output.
