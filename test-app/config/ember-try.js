1c0077d83855594ceaa15847113f67178ae1c7c3
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-11-14 12:54:22 -0500

    Downgrade @ember/test-helpers for 4.4 and 3.28

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -10,6 +10,7 @@ module.exports = async function () {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
+            '@ember/test-helpers': '^2.7.0',
             'ember-resolver': '^10.0.0',
             'ember-source': '~3.28.0',
             'ember-cli': '~4.12.0',
@@ -20,6 +21,7 @@ module.exports = async function () {
         name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
+            '@ember/test-helpers': '^2.7.0',
             'ember-resolver': '^10.0.0',
             'ember-source': '~4.4.0',
           },
