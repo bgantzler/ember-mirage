123196cf56db5bba6e6cd291abd636317192c3c1
Chris Manson <chris@manson.ie>
2025-06-26 16:31:45 +0100

    move qunit config out of module scope

--- ember-mirage/src/test-support.js
+++ ember-mirage/src/test-support.js
@@ -1,10 +1,1 @@
-import { dependencySatisfies } from '@embroider/macros';
-
-if (dependencySatisfies('ember-qunit', '*')) {
-  window.QUnit.config.urlConfig.push({
-    id: 'mirageLogging',
-    label: 'Mirage logging',
-  });
-}
-
 export { setupMirage } from './test-support/setup-mirage';
